d1c336b7f8b30c849bd8e2903a37a9b0ce7902da
hanliumaozhi <hanliumaozhi@gmail.com>
2014-10-13 04:54:09 +0800

    update

--- extend.py
+++ extend.py
@@ -2,7 +2,7 @@
 import sqlite3
 
 def init_db():
-    cx = sqlite3.connect("../myWeb/app/db/mc.db")
+    cx = sqlite3.connect("./app/db/mc.db")
     cu=cx.cursor()
     cu.execute("create table MC_user_online_time (user_name text primary key, pre_total_time integer, current_time_length integer)")
     cu.close()
