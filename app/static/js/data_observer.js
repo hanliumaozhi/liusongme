bf23476666f570533f28c673dd179dd00c241997
hanliumaozhi <hanliumaozhi@gmail.com>
2014-11-19 21:50:17 +0800

    fix

--- app/static/js/data_observer.js
+++ app/static/js/data_observer.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 		var update_str = '<tr><td class="col-md-6 text-center">图例</td></tr><tr><th class="col-md-6 text-center"><div id="myGraph"></div></th></tr>';
 		console.log(obj);
 		$("#myTable").empty();
+		$("#myTable").append(update_str);
 		lc = new LineChart({
 			parent: '#myGraph',
 			x_scale: d3.time.scale()
