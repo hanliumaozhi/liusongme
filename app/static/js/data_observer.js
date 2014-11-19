8f901bbeff66bf58949f92db369d6ed202dc0291
hanliumaozhi <hanliumaozhi@gmail.com>
2014-11-19 21:36:52 +0800

    fix

--- app/static/js/data_observer.js
+++ app/static/js/data_observer.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 	$.post("/data_observer", {type:1}, function(data){
 		var obj = eval('(' + data + ')');
+		var update_str = '<tr><td class="col-md-6 text-center">图例</td></tr><tr><th class="col-md-6 text-center"><div id="myGraph"></div></th></tr>';
+		console.log(obj);
 		$("#myTable").empty();
 		lc = new LineChart({
-			parent: '#myTable',
+			parent: '#myGraph',
 			x_scale: d3.time.scale()
 		});
 		lc.for([obj]).plot();
